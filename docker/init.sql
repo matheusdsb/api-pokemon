bcff1c5137947e400a450e6575429d990726359c
Matheus Sabino <matheusdsb@gmail.com>
2020-11-30 03:13:21 -0300

    correção no campo tipo no script de geração da tabela

--- docker/init.sql
+++ docker/init.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE TABLE jazida.dbo.Pokemons (
 	id int IDENTITY(1,1) PRIMARY KEY,
-	iipo nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	tipo nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	treinador nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	nivel int NOT NULL
 ); 
