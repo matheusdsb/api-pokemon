6e868f78b32e5396795b3083a870e42aefaa6780
Matheus Sabino <matheusdsb@gmail.com>
2020-11-30 14:49:03 -0300

    métodos de criar, editar, deletar pokemons

--- server/src/apis/pokemon.api.js
+++ server/src/apis/pokemon.api.js
@@ -1,3 +1,10 @@
+const { 
+    tiposPermitidos, 
+    carregarDadosRequest, 
+    validarDadosCriacao, 
+    validarDadosEdicao
+} = require('../helpers/pokemon.helper')
+
 module.exports = (app, sequelize ) => {
     app.get('/pokemons',  async (req, res) => {
         try {              
@@ -9,16 +16,79 @@ module.exports = (app, sequelize ) => {
         }
     }),
     app.post('/pokemons',  async (req, res) => {
-        try {              
+        try {
+            const input = carregarDadosRequest(req.body)
+            validarDadosCriacao(input)
+
             const { Pokemon } = sequelize
             const pokemon = await Pokemon.create({
-                tipo: req.body.tipo,
-                treinador: req.body.treinador,
+                tipo: input.tipo,
+                treinador: input.treinador,
                 nivel: 1
             })
             res.json(pokemon)
         } catch(e) {
             res.status(400).send(e.message)
         }
-    })    
+    }),  
+    app.put('/pokemons/:id',  async (req, res) => {
+        try {
+            const id = req.params.id
+
+            if(!id) {
+                res.status(404).send('ID do pokemon não informado')
+            }
+
+            const { Pokemon } = sequelize
+            const pokemon = await Pokemon.findByPk(id)
+
+            if(!pokemon) {
+                res.status(404).send('Pokemon não encontrado')
+            }
+
+            const input = carregarDadosRequest(req.body)
+            validarDadosEdicao(input)
+            
+            await pokemon.update({ treinador: input.treinador })
+
+            res.status(204).send()
+
+        } catch(e) {
+            res.status(400).send(e.message)
+        }
+    }),
+    app.delete('/pokemons/:id',  async (req, res) => {
+        try {
+            const id = req.params.id
+
+            const { Pokemon } = sequelize
+            await Pokemon.destroy({
+                where: {
+                    id: id
+                }
+            })
+
+            res.status(204).send()
+
+        } catch(e) {
+            res.status(400).send(e.message)
+        }
+    }),
+    app.get('/pokemons/:id',  async (req, res) => {
+        try {
+            const id = req.params.id
+
+            const { Pokemon } = sequelize
+            const pokemon = await Pokemon.findByPk(id)
+
+            if(!pokemon) {
+                res.status(404).send('Pokemon não encontrado')
+            }
+
+            res.json(pokemon)
+
+        } catch(e) {
+            res.status(400).send(e.message)
+        }
+    })  
 }
