20ba483fd799d5e791fc1d15d35acd041fa7372e
Matheus Sabino <matheusdsb@gmail.com>
2020-11-30 00:13:31 -0300

    geração da imagem do banco de dados e tabelas

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pass=${MSSQL_SA_PASSWORD}
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -i init.sql &
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $pass \
+    -i init.sql &
 /opt/mssql/bin/sqlservr
--- docker/init.sql
+++ docker/init.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE jazida;
-go
+GO
+
+CREATE TABLE jazida.dbo.Pokemons (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	iipo nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	treinador nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	nivel int NOT NULL
+); 
+GO
